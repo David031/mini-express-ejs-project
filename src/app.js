893559de43f8a3b980717e90358338e7b16f0829
David031 <davidtsang031@gmail.com>
2021-11-22 12:25:38 +0800

    add files with placeholder

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 import express from "express";
 import { login } from "./connect.js";
-
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => {
-  login("david","abc")
-  res.status(200).send({start: 'Hello World!'})
-})
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/main', {
+    mascots: mascots,
+    tagline: tagline
+  });
+});
+
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
